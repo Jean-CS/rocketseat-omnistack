04f98e3da571376f8c58458f65955ee41783d3c0
Jean Santos <jeansantosc@gmail.com>
2019-04-09 22:16:19 -0300

    add file upload and box relations

--- backend/src/controllers/FileController.js
+++ backend/src/controllers/FileController.js
@@ -1,10 +1,19 @@
+const Box = require('../models/Box');
 const File = require('../models/File');
 
 class FileController {
     async store(req, res) {
-        console.log(req.file);
+        const file = await File.create({
+            title: req.file.originalname,
+            // name of the file, from multer
+            path: req.file.key,
+        });
 
-        return res.send('Ok');
+        const box = await Box.findById(req.params.id);
+        box.files.push(file);
+        await box.save();
+
+        return res.json(file);
     }
 }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,7 +8,9 @@ const BoxController = require('./controllers/BoxController');
 const FileController = require('./controllers/FileController');
 
 routes.post('/boxes', BoxController.store);
+routes.get('/boxes/:id', BoxController.show);
+
 // 'file' is the name of the file from the client
-routes.post('/files', multer(multerConfig).single('file'), FileController.store);
+routes.post('/boxes/:id/files', multer(multerConfig).single('file'), FileController.store);
 
 module.exports = routes;
