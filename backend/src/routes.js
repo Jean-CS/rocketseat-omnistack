9385387d8b660172f14bfb06b088fbfca39628c4
Jean Santos <jeansantosc@gmail.com>
2019-04-09 22:08:16 -0300

    file controller and route for adding files

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const multer = require('multer');
+const multerConfig = require('./config/multer');
 
 const routes = express.Router();
 
 const BoxController = require('./controllers/BoxController');
+const FileController = require('./controllers/FileController');
 
 routes.post('/boxes', BoxController.store);
+// 'file' is the name of the file from the client
+routes.post('/files', multer(multerConfig).single('file'), FileController.store);
 
 module.exports = routes;
