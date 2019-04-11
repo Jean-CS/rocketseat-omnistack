d7be78f4e565366acd34041737a425eb82c1594c
Jean Santos <jeansantosc@gmail.com>
2019-04-11 00:22:36 -0300

    add axios and initial api call

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
 
 import './styles.css';
 
 export default class Main extends Component {
+    state = {
+        newBox: '',
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        const res = await api.post('boxes', {
+            title: this.state.newBox,
+        });
+
+        console.log(res.data);
+    };
+
+    handleInputChange = e => {
+        this.setState({
+            newBox: e.target.value,
+        });
+    };
+
     render() {
         return (
             <div id="main-container">
-                <form action="">
-                    <i class="fas fa-magnet " />
-                    <input placeholder="Criar um box" />
+                <form onSubmit={this.handleSubmit}>
+                    <i className="fas fa-magnet " />
+                    <input placeholder="Criar um box" value={this.state.newBox} onChange={this.handleInputChange} />
                     <button type="submit">Criar</button>
                 </form>
             </div>
