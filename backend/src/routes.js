ac2f89482c54a50f3504e6009924bbd730235ba1
Jean Santos <jeansantosc@gmail.com>
2019-04-11 01:38:00 -0300

    fix frotend path

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -17,4 +17,11 @@ routes.get('/boxes/:id', BoxController.show);
 // 'file' is the name of the file from the client
 routes.post('/boxes/:id/files', multer(multerConfig).single('file'), FileController.store);
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+routes.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+    console.log('routes get *');
+});
+
 module.exports = routes;
